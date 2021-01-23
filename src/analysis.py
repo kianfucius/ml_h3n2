daf4fdff37388458d611be3195f78d6ef8d70ad4
Kian Salamzadeh <kian.salamzadeh@hotmail.com>
2021-01-23 00:18:42 -0500

    Added args and restructured some files - added path argument to specify data directory - added extension argument to specify data extension - fixed a bug in read_gisaid_file which was causing   it to only read the first segment of a strain

--- src/analysis.py
+++ src/analysis.py
@@ -1,33 +1,66 @@
+# Python imports
+import argparse
+import os
 # Third party imports
 import matplotlib.pyplot as plt
 # My imports
 import helpers
 
-def analyze_segment_count():
-    print('We are going to collect all the data located in ml_h3n2/sample_data/*.fasta')
-    sample_data = helpers.get_all_data("../sample_data", ".fasta")
+def analyze_segment_count(datapath, ext):
+    # Collect data
+    print('Collecting data from ml_h3n2/sample_data/*.fasta')
+    sample_data = helpers.get_all_data(directory=datapath, extension=ext)
     print(f'Collected {len(sample_data)} unique strains')
 
+    # Count segments
     segment_count={'PB2':0, 'PB1':0, 'PA':0, 'HA':0, 'NP':0, 'NA':0, 'MP':0, 'NS':0, 'HE':0, 'P3':0}
     for strain in sample_data.values():
         for key in strain.segments:
             segment_count[key] += 1
-
+    print()
     print('Found the following number of occurences for each segment:')
     print(segment_count)
+    print()
 
+    # Set graph data
     plt.bar(range(len(segment_count)), list(segment_count.values()), align='center')
     plt.xticks(range(len(segment_count)), list(segment_count.keys()))
-    plt.title('# of Occurrences per Segment')
+    # Set graph titles and labels
+    plt.title('# of Occurrences per Segment\nInternational data up to 01-22-2021')
     plt.ylabel('# of Segments')
     plt.xlabel('Segment Name')
+    # Generate graph
     plt.savefig('../figures/segment_count')
     print('Figure saved to ml_h3n2/figures/segment_count.png')
     plt.show()
     print(f'We can see that {segment_count["HA"]} of {len(sample_data)} samples include the hemaggluttinin (HA) segment.')
+    print()
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', default='../sample_data', type=dir_path,
+        help="Path to directory containing virus data.")
+    parser.add_argument('-e', default='.fasta', type=is_extension,
+        help="File extension for data files.")
+
+    return parser.parse_args()
+
+def dir_path(string):
+    if os.path.isdir(string):
+        return string
+    else:
+        argparse.ArgumentError(f'Invalid path {string}')
+
+def is_extension(string):
+    if string[0] == '.' and string[1:].isalpha():
+        return string
+    else:
+        argparse.ArgumentError(f'Invalid extension {string}')
 
 def main():
-    analyze_segment_count()
+    parsed_args = parse_arguments()
+
+    analyze_segment_count(parsed_args.p, parsed_args.e)
 
 if __name__ == "__main__":
     main()
--- src/helpers.py
+++ src/helpers.py
@@ -17,17 +17,19 @@ def read_gisaid_file(filename):
     # Index from 1 onwards because the first '>' throws things off a bit.
     segments = file_contents.split('>')[1:]
     for segment in segments:
-        temp_name, temp_date, temp_segname, temp_segdata = segment.split('|')
-        # We are using the name as a key.
-        # If it doesn't exist, initialize it.
-        if not temp_name in allData:
-            tempStrain = strain_class.DnaData(temp_name, temp_date)
-            allData[temp_name] = tempStrain
+        try:
+            temp_name, temp_date, temp_segname, temp_segdata = segment.split('|')
+            # We are using the name as a key.
+            # If it doesn't exist, initialize it.
+            if not temp_name in allData:
+                tempStrain = strain_class.DnaData(temp_name, temp_date)
+                allData[temp_name] = tempStrain
             allData[temp_name].segments[temp_segname] = temp_segdata
-
+        except:
+            print(f'Found bad data in {filename}')
     return allData
 
-def get_all_data(directory, extension):
+def get_all_data(directory="../sample_data", extension=".fasta"):
     allData = {}
     for file in os.listdir(directory):
         if file.endswith(extension):
